2730c3f78b37d261a6f588aeb286c9ac64094030
JorgeDeLaRosa04 <jorgediegojorgediego7@gmail.com>
2026-04-09 18:54:16 -0500

    Creación de la tabla users, roles y user_role

---
+++ CubeGridLab/CubeGridLab/src/main/java/pe/edu/upc/cubegridlab/entities/Role.java
@@ -0,0 +1,37 @@
+package pe.edu.upc.cubegridlab.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idRole;
+    @Column(name = "nameRole", length = 20, nullable = false)
+    private String nameRole;
+
+    public Role() {
+    }
+
+    public Role(int idRole, String nameRole) {
+        this.idRole = idRole;
+        this.nameRole = nameRole;
+    }
+
+    public int getIdRole() {
+        return idRole;
+    }
+
+    public void setIdRole(int idRole) {
+        this.idRole = idRole;
+    }
+
+    public String getNameRole() {
+        return nameRole;
+    }
+
+    public void setNameRole(String nameRole) {
+        this.nameRole = nameRole;
+    }
+}
